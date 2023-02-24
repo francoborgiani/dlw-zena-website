555bb120167cc6b4df3ada2eac419a04316ae9f8
Franco Borgiani <92275958+francoborgiani@users.noreply.github.com>
2023-02-24 01:47:49 -0300

    add connection with snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 
 import streamlit
+import snowflake.connector
 
 streamlit.header("Hello")
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+
+streamlit.text("Hello from snowflake")
+streamlit.text(my_data_row)
